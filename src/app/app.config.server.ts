cb50a14fb9cad18357f783c8e91430d02c8e85c9
emre1702 <emre1702@live.de>
2023-08-15 00:42:25 +0200

    Test more more

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,7 +1,6 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import {
   provideServerRendering,
-  ɵSERVER_CONTEXT as SERVER_CONTEXT,
 } from '@angular/platform-server';
 import { appConfig } from './app.config';
 import { provideNoopAnimations } from '@angular/platform-browser/animations';
@@ -10,7 +9,6 @@ import { provideClientHydration } from '@angular/platform-browser';
 const serverConfig: ApplicationConfig = {
   providers: [
     provideServerRendering(),
-    { provide: SERVER_CONTEXT, useValue: 'ssr-analog' },
     provideClientHydration(),
     provideNoopAnimations(),
   ],
