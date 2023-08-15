1f8e7c39c1d0508bda987ee57a4d5d0f7b1ad00a
emre1702 <emre1702@live.de>
2023-08-15 02:06:04 +0200

    Try more more more

--- vite.config.ts
+++ vite.config.ts
@@ -2,46 +2,39 @@
 
 import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import analog from '@analogjs/platform';
-import * as path from 'path';
 import { visualizer } from 'rollup-plugin-visualizer';
+import tsConfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   publicDir: 'src/assets',
-  server: {
-    host: '127.0.0.1',
-  },
   optimizeDeps: {
     include: ['@angular/common', '@angular/forms', 'isomorphic-fetch'],
   },
   ssr: {
     noExternal: [
-      '@analogjs/trpc/**',
-      '@angular/cdk/**',
-      '@ng-icons/**',
-      'ngx-scrollbar/**',
+      '@analogjs/trpc',
+      '@angular/cdk'
     ],
   },
   build: {
     target: ['es2020'],
   },
-  resolve: {
-    mainFields: ['module'],
-    alias: {
-      '~': path.resolve(__dirname, './src'),
-      'src/': path.resolve(__dirname, './src'),
-    },
-  },
   plugins: [
     analog({
       vite: {
         inlineStylesExtension: 'scss',
-        tsconfig: path.resolve(__dirname, './tsconfig.app.json'),
       },
       nitro: {
         preset: 'vercel',
         serveStatic: false,
       },
+      prerender: {
+        routes: ['/'],
+      },
+    }),
+    tsConfigPaths({
+      root: './'
     }),
     visualizer(),
     splitVendorChunkPlugin(),
@@ -52,7 +45,7 @@ export default defineConfig(({ mode }) => ({
     setupFiles: ['src/test.ts'],
     include: ['**/*.spec.ts'],
     cache: {
-      dir: `../../node_modules/.vitest`,
+      dir: `./node_modules/.vitest`,
     },
   },
   define: {
