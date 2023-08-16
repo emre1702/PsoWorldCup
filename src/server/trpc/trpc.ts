8ee81ca5f32a38fe37e2e3a2ec5a0596971c7683
emre1702 <emre1702@live.de>
2023-08-16 18:31:06 +0200

    Improve code

--- src/server/trpc/trpc.ts
+++ src/server/trpc/trpc.ts
@@ -19,43 +19,31 @@ export const publicProcedure = t.procedure;
 export const router = t.router;
 export const middleware = t.middleware;
 
-export const protectedInputProcedure = <T extends ZodTypeAny>(input: T) =>
-  publicProcedure.input(zodInputWithAuth(input)).use(async (opts) => {
-    console.log(opts);
-    const { token } = opts.input;
-    if (!token || token === '-') throw new TRPCError({ code: 'UNAUTHORIZED' });
+const oauth = createDiscordOAuth2();
+async function getUserMiddleware(opts: {
+  ctx: { user?: { id: string } };
+  input: { token?: string };
+  next: (opts: { ctx: any }) => Promise<any>;
+}) {
+  const { token } = opts.input;
+  if (!token || token === '-') throw new TRPCError({ code: 'UNAUTHORIZED' });
 
-    try {
-      const oauth = createDiscordOAuth2();
-      opts.ctx.user = await oauth.getUser(token);
-    } catch (e) {
-      throw new TRPCError({
-        code: 'UNAUTHORIZED',
-        message: (typeof e === 'object' && e && 'message' in e
-          ? e.message
-          : e) as string,
-      });
-    }
-    return opts.next(opts);
-  });
+  try {
+    opts.ctx.user = await oauth.getUser(token);
+  } catch (e) {
+    throw new TRPCError({
+      code: 'UNAUTHORIZED',
+      message: (typeof e === 'object' && e && 'message' in e
+        ? e.message
+        : e) as string,
+    });
+  }
+  return opts.next(opts);
+}
+
+export const protectedInputProcedure = <T extends ZodTypeAny>(input: T) =>
+  publicProcedure.input(zodInputWithAuth(input)).use(getUserMiddleware);
 
 export const protectedProcedure = publicProcedure
   .input(zodInputWithAuth(z.null().optional()))
-  .use(async (opts) => {
-    console.log(opts);
-    const { token } = opts.input;
-    if (!token || token === '-') throw new TRPCError({ code: 'UNAUTHORIZED' });
-
-    try {
-      const oauth = createDiscordOAuth2();
-      opts.ctx.user = await oauth.getUser(token);
-    } catch (e) {
-      throw new TRPCError({
-        code: 'UNAUTHORIZED',
-        message: (typeof e === 'object' && e && 'message' in e
-          ? e.message
-          : e) as string,
-      });
-    }
-    return opts.next(opts);
-  });
+  .use(getUserMiddleware);
